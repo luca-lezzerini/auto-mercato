5ee3f6557135b54ef089824c5258104889bbeaac
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-03-05 09:18:43 +0100

    Commit iniziale, applicazione vuota

---
+++ server/src/main/java/it/sirfin/automercato/AutomercatoApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.automercato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomercatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomercatoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/it/sirfin/automercato/AutomercatoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.automercato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomercatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
